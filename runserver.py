85abb3816a736f9cb57bfc6bbb039736efff1ca1
Salimane Adjao Moustapha <salimane@gmail.com>
2011-11-21 17:26:00 +0800

    remove one level deep of project folder

--- runserver.py
+++ runserver.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
 from project import app
 from project.models import printer
 
 if __name__=='__main__':
-    app.run('0.0.0.0',debug=True)
+  port = int(os.environ.get("PORT", 8080))
+  app.run('0.0.0.0',debug=True, port=port)
