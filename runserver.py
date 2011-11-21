f10ff50f0bcd3fcde06c0df231a21cf9a07b1b0d
Salimane Adjao Moustapha <salimane@gmail.com>
2011-11-21 18:31:06 +0800

    add depedency to readme

--- runserver.py
+++ runserver.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 from project import app
-from project.models import printer
 
-if __name__=='__main__':
+if __name__ == '__main__':
   port = int(os.environ.get("PORT", 8080))
-  app.run('0.0.0.0',debug=True, port=port)
+  app.run('0.0.0.0', debug=True, port=port)
