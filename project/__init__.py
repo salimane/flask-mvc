309b4295ab119f2665844036364ae9969aef196b
Salimane Adjao Moustapha <me@salimane.com>
2012-10-17 18:29:34 +0800

    add flask debugtoolbar

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 __version__ = '0.1'
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 app = Flask('project')
 app.config['SECRET_KEY'] = 'random'
+app.debug = True
+toolbar = DebugToolbarExtension(app)
 from project.controllers import *
